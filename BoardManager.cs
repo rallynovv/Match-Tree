85f01aebf2f7200c1bb5f955bb56f99fd14e579f
rallynovv <75323384+rallynovv@users.noreply.github.com>
2021-09-29 12:43:24 +0700

    Add files via upload

---
+++ BoardManager.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static BoardManager _instance = null;
+
+    public static BoardManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<BoardManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: BoardManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    [Header("Board")]
+    public Vector2Int size;
+    public Vector2 offsetTile;
+    public Vector2 offsetBoard;
+
+    [Header("Tile")]
+    public List<Sprite> tileTypes = new List<Sprite>();
+    public GameObject tilePrefab;
+
+    
+    public bool IsAnimating { get; set; }
+
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+    private TileController[,] tiles;
+
+    private void Start()
+    {
+        Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
+        CreateBoard(tileSize);
+
+        IsAnimating = false;
+    }
+
+    #region Board Generator
+
+    private void CreateBoard(Vector2 tileSize)
+    {
+        tiles = new TileController[size.x, size.y];
+
+        Vector2 totalSize = (tileSize + offsetTile) * (size - Vector2.one);
+
+        startPosition = (Vector2)transform.position - (totalSize / 2) + offsetBoard;
+        endPosition = startPosition + totalSize;
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                TileController newTile = Instantiate(tilePrefab, new Vector2(startPosition.x + ((tileSize.x + offsetTile.x) * x), startPosition.y + ((tileSize.y + offsetTile.y) * y)), tilePrefab.transform.rotation, transform).GetComponent<TileController>();
+                tiles[x, y] = newTile;
+                //get no match id
+                List<int> possibleId = GetStartingPossibleIdList(x, y); 
+                int newId = possibleId[Random.Range(0, possibleId.Count)];
+
+                newTile.ChangeId(newId, x, y);
+
+            }
+        }
+    }
+    private List<int> GetStartingPossibleIdList(int x, int y)
+    {
+        List<int> possibleid = new List<int>();
+
+        for (int i = 0; i < tileTypes.Count; i++)
+        {
+            possibleid.Add(i);
+        }
+        
+        if (x > 1 && tiles[x - 1, y].id == tiles[x - 2, y].id)
+        {
+            possibleid.Remove(tiles[x - 1, y].id);
+        }
+
+        if (y > 1 && tiles[x, y - 1].id == tiles[x, y - 2].id)
+        {
+            possibleid.Remove(tiles[x, y - 1].id);
+        }
+        return possibleid;
+
+        Generate;
+
+        #region Swapping 
+
+        public IEnumerator SwapTilePosition(TileController a, TileController b, System.Action onComplete)
+        {
+            Vector2Int indexA = GetTileIndex(a);
+            Vector2Int indexB = GetTileIndex(b);
+
+            tiles[indexA.x, indexA.y] = b;
+            tiles[indexB.x, indexA.y] = a;
+
+            a.ChangeId(a.id, indexB.x, indexB.y);
+            b.ChangeId(b.id, indexA.x, indexA.y);
+
+            bool isRoutineACompleted = false;
+            bool isRoutineBCompleted = false;
+
+            StartCoroutine(a.MoveTilePosition(GetIndexPosition(indexB), () => { isRoutineACompleted = true; }));
+            StartCoroutine(a.MoveTilePosition(GetIndexPosition(indexA), () => { isRoutineBCompleted = true; }));
+
+            yield return new WaitUntil(() => { return isRoutineACompleted && isRoutineBCompleted; });
+
+            onCompleted?.Invoke();
+        }
+
+        #endregion
+
+
+        #region Helper
+
+        public Vector2Int GetTileIndex(TileController tile)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                for (int y = 0; y < size.y; y++)
+                {
+                    if (tile == tiles[x, y]) return new Vector2Int(x, y);
+                }
+            }
+
+            return new Vector2Int(-1, -1);
+        }
+
+        #endregion
+
+    }
+
+    public Vector2 GetIndexPosition(Vector2Int index)
+    {
+        Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
+        return new Vector2((startPosition.x + ((tileSize.x + offsetTile.x) * index.x), startPosition.y + ((tileSize.y + offsetTile.y)) * index.y));
+    }
+}
+    
+
---
+++ TileController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f);
+    private static Color normalColor = Color.white;
+
+    private static float moveDuration = 0.05f;
+
+    private static TileController previousSelected = null;
+
+    public int id;
+
+    private BoardManager board;
+    private SpriteRenderer render;
+    private bool isSelected = false;
+
+    private void Awake()
+    {
+        board = BoardManager.Instance;
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        // Non Selectable conditions
+        if (render.sprite == null || board.IsAnimating)
+        {
+            
+            return;
+        }
+        //Already Selected this tile?
+        if (isSelected)
+        {
+            Deselect();
+        }
+        //
+        else
+        {
+            if(previousSelected == null)
+            {
+                Select();
+            }
+            //try swaping
+            else
+            {
+                //swap tile
+                SwapTile(previousSelected);
+
+                // if cant swap than change selected 
+                previousSelected.Deselect();
+                Select();
+              
+            }
+
+        }
+    }
+
+    public void ChangeId(int id, int x, int y)
+    {
+        render.sprite = board.tileTypes[id];
+        this.id = id;
+
+        name = "TILE_" + id + " (" + x + ", " + y + ")";
+    }
+
+    private void Select()
+    {
+        isSelected = true;
+        render.color = selectedColor;
+        previousSelected = this;
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        render.color = normalColor;
+        previousSelected = null;
+    }
+
+    public void SwapTile(TileController otherTile, System .Action onCompleted)
+    {
+        StartCoroutine(board.SwapTilePosition(this, otherTile, onCompleted));
+    }
+
+    public  IEnumerator MoveTilePosition(Vector2 targetPosition, System.Action onComplete)
+    {
+        Vector2 startPosition = transform.position;
+        float duration = moveDuration;
+        float time = 0.0f;
+
+        //run animation on next frame for safety reason;
+        yield return new WaitForEndOfFrame();
+
+        while (time < duration)
+        {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = targetPosition;
+
+        onComplete?.Invoke();
+    }
+}
+
